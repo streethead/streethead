0e5585bb4b4de9b118b97477096139a0335891b9
Maddane Motaquillah <maddane.info@gmail.com>
2015-10-18 14:26:27 +0100

    Refectoring Modal + Added animations

--- src/app/components/modal/modals-container.directive.js
+++ src/app/components/modal/modals-container.directive.js
@@ -9,7 +9,7 @@
             link: link,
             templateUrl: 'app/components/modal/modals-container.html',
             scope: {},
-            controller: controller,
+            controller: controller, /* As a way to communicate with sub-directives */
             bindToController: true,
             controllerAs: 'vm'
         };
@@ -24,8 +24,8 @@
 
         function link(scope, elm, attrs, vm) {
             vm.states = {
-                eventsAttached: false,
-                loaded: false
+                isEventsAttached: false,
+                isLoaded: undefined
             };
 
             vm.modal = {
@@ -35,19 +35,21 @@
             };
 
             vm.close = close;
-            vm.loading = LoadingExternalTemplate;
+            vm.onLoad = whenExternalIsTemplateLoaded;
 
             $rootScope.$on('modal.open', function(event, modal) { open(modal); });
             $rootScope.$on('modal.close', function(event, modal) { close(modal); });
 
             function open(modal) {
                 vm.modal.templateUrl = modal.templateUrl;
+                vm.states.isLoaded = false;
                 vm.modal.isOpen = true;
+                vm.modal.title = undefined;
                 togglePageScroll(true);
                 /* When successive modals are opened */
-                if(!vm.states.eventsAttached) {
+                if(!vm.states.isEventsAttached) {
                     attachCloseEvents();
-                    vm.states.eventsAttached = true;
+                    vm.states.isEventsAttached = true;
                 }
             }
 
@@ -55,13 +57,14 @@
                 if(vm.modal.isOpen) {
                     vm.modal.templateUrl = undefined;
                     vm.modal.isOpen = false;
+                    vm.modal.title = undefined;
                     togglePageScroll(false);
                     detachCloseEvents();
-                    vm.states.eventsAttached = false;
+                    vm.states.isEventsAttached = false;
                 }
             }
 
-            function LoadingExternalTemplate() { vm.states.loaded = true; }
+            function whenExternalIsTemplateLoaded() { vm.states.isLoaded = true; }
 
             function attachCloseEvents() {
                 elm.on('click', clickAwayCallback);
